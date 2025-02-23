e3bc4bc8dbc3f5198e07f876f0f102287aefe37e
genivaldoperez <153784160+genivaldoperez@users.noreply.github.com>
2025-02-23 12:54:08 +0000

    Main

--- super_trunfo.c
+++ super_trunfo.c
@@ -13,7 +13,7 @@ int main()
     // Criar um programa em C que permita ao usuário inserir os dados de duas cartas do Super Trunfo.
     // A combinação da letra do estado e o número da cidade define o código da carta (por exemplo, A01, A02, B01, B02).
    
-    // Definindo as variáveis da carta código A01
+    // Definindo as variáveis da carta 1
     char estado_a = 'A';
     char codigo_a01[4] = "A01";
     char nome_cidade_a01[50];
@@ -22,7 +22,7 @@ int main()
     float pib_a01;
     int nPontosTuristicos_a01;
 
-    // Definindo as variáveis da carta código A02
+    // Definindo as variáveis da carta 2
     char codigo_a02[4] = "A02";
     char nome_cidade_a02[50];
     int populacao_a02;
@@ -35,9 +35,9 @@ int main()
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
 
     // Cadastrando as cartas das cidades do Estado A:
-    printf("Ola jogador, agora iremos cadatrar as cartas para as cidades do Estado A\n");
+    printf("Ola jogador, insira os dados da Carta 1\n");
 
-    // Cadastrando a Cidade 01 do Estado A:
+    // Cadastrando a carta 1: Cidade 01 do Estado A:
     printf("\n");
     printf("Digite o nome da cidade 01 do Estado A.\n");
     scanf("%s", nome_cidade_a01);
@@ -50,7 +50,7 @@ int main()
     printf("Digite o número de pontos turisticos da cidade de %s\n", nome_cidade_a01);
     scanf("%d", &nPontosTuristicos_a01);
 
-    // Cadastrando a Cidade 02 do Estado A:
+    // Cadastrando a carta 2: Cidade 02 do Estado A:
     printf("\n");
     printf("Digite o nome da cidade 02 do Estado A.\n");
     scanf("%s", nome_cidade_a02);
@@ -72,17 +72,19 @@ int main()
 
     // Imorimindo a carta A01
     printf("\n================================================\n");
+    printf("Carta 1:");
     printf("Estado: %c\n", estado_a);
     printf("Código da Carta: %s\n", codigo_a01);
     printf("Nome da Cidade: %s\n", nome_cidade_a01);
     printf("População: %d\n", populacao_a01);
-    printf("Área: %2f\n", area_a01);
+    printf("Área: %2f Km^2\n", area_a01);
     printf("PIB: %2f\n", pib_a01);
     printf("Número de Pontos Turísticos: %d\n", nPontosTuristicos_a01);
-    printf("================================================\n");
+    printf("==================================================\n");
 
     // Imorimindo a carta A02
     printf("\n================================================\n");
+    printf("Carta 2:");
     printf("Estado: %c\n", estado_a);
     printf("Código da Carta: %s\n", codigo_a02);
     printf("Nome da Cidade: %s\n", nome_cidade_a02);
@@ -90,5 +92,5 @@ int main()
     printf("Área: %2f\n", area_a02);
     printf("PIB: %2f\n", pib_a02);
     printf("Número de Pontos Turísticos: %d\n", nPontosTuristicos_a02);
-    printf("================================================\n");
+    printf("==================================================\n");
 }
