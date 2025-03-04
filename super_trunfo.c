0a1055ad90a8b10d6429537dc73680723dd6e2b7
genivaldoperez <153784160+genivaldoperez@users.noreply.github.com>
2025-03-04 22:47:23 +0000

    Tema 2: Desafio Nível Aventureiro

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
+float divide(float x, float y); // Função para cálculo da densidade populacional
+int compara(float x, float y); // Função para comparação dos atributos
+void imprime_comparacao(int x); // Função para imprimir o resultado da comparação
 
-float divide(float x, float y);
-int compara(float x, float y);
-void imprime_comparacao(int x);
+// Definição de variáveis globais
 float super_poder_1, super_poder_2;
+char nome_cidade_a01[50];
+char nome_cidade_a02[50];
 
 int main() 
 {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-
-    // Criar um programa em C que permita ao usuário inserir os dados de duas cartas do Super Trunfo.
-    // A combinação da letra do estado e o número da cidade define o código da carta (por exemplo, A01, A02, B01, B02).
-   
     // Definindo as variáveis da carta 1
     char estado_01[2];
     char codigo_a01[4];
-    char nome_cidade_a01[50];
     unsigned long int populacao_a01;
     float area_a01;
     float pib_a01;
@@ -30,15 +22,10 @@ int main()
     // Definindo as variáveis da carta 2
     char estado_02[2];
     char codigo_a02[4];
-    char nome_cidade_a02[50];
     unsigned long int populacao_a02;
     float area_a02;
     float pib_a02;
     int nPontosTuristicos_a02;
-
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
 
     // Cadastrando as cartas:
     printf("\nOla jogador, insira os dados da Carta 1\n");
@@ -78,29 +65,25 @@ int main()
     printf("Digite o número de pontos turisticos da cidade de %s\n", nome_cidade_a02);
     scanf("%d", &nPontosTuristicos_a02);
 
-    //Pib per Capita e Densidade Populacional
+    //Cálculo do Pib per Capita e Densidade Populacional
     float perCapita_1 = divide(pib_a01, (float)populacao_a01);
     float densidade_1 = divide((float)populacao_a01, area_a01);
 
     float perCapita_2 = divide(pib_a02, (float)populacao_a02);
     float densidade_2 = divide((float)populacao_a02, area_a02);
 
-    //Calculo Super Poder
+    //Calculo do Super Poder
     float inverso_densidade_1 = 1 / densidade_1;
     super_poder_1 = (float)populacao_a01 +  area_a01 + pib_a01 + (float)nPontosTuristicos_a01 + perCapita_1 + inverso_densidade_1;
-
     float inverso_densidade_2 = 1 / densidade_2;
     super_poder_2 = (float)populacao_a02 +  area_a02 + pib_a02 + (float)nPontosTuristicos_a02 + perCapita_2 + inverso_densidade_2;
 
 
     // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
     printf("\n\n");
     printf("Relação das Cartas Cadastradas:\n");
 
-    // Imorimindo a carta A01
+    // Imorimindo a carta 1
     printf("\n================================================\n");
     printf("Carta 1:\n");
     printf("Estado: %s\n", estado_01);
@@ -115,7 +98,7 @@ int main()
     printf("Super Poder: %.2f\n", super_poder_1);
     printf("==================================================\n");
 
-    // Imorimindo a carta A02
+    // Imorimindo a carta 2
     printf("\n================================================\n");
     printf("Carta 2:\n");
     printf("Estado: %s\n", estado_02);
@@ -131,70 +114,102 @@ int main()
     printf("==================================================\n");
 
 
-    // Comparando as cartas
+    // Váriáveis com os resultados da comparação dos atributos
     int resultado_populacao = compara((float)populacao_a01, (float)populacao_a02);
     int resultado_area = compara( area_a01, area_a02 );
     int resultado_pib = compara(pib_a01, pib_a02);
     int resultado_pontosTuristicos = compara((float)nPontosTuristicos_a01, (float)nPontosTuristicos_a02);
-    int resultado_densidade = compara(densidade_1, densidade_2);
+    int resultado_densidade = compara(densidade_2, densidade_1);
     int resultado_percapita = compara(perCapita_1, perCapita_2);
     int resultado_superPoder = compara(super_poder_1, super_poder_2);
 
-    /* usado no tema 1
-    // Imprimindo o resultado da comparação das cartas
-    printf("\nComparação de Cartas:\n");
-    printf("População: ");
-    imprime_comparacao( resultado_populacao );
-    printf("Área: ");
-    imprime_comparacao( resultado_area );
-    printf("PIB: ");
-    imprime_comparacao( resultado_pib );
-    printf("Pontos Turísticos: ");
-    imprime_comparacao( resultado_pontosTuristicos );
-    printf("Densidade Populacional: ");
-    imprime_comparacao( resultado_densidade);
-    printf("PIB per Capita: ");
-    imprime_comparacao( resultado_percapita );
-    printf("Super Poder: ");
-    imprime_comparacao( resultado_superPoder );*/
-
-    // Início desafio do tema 2
-
-    // Atributo escolhido para comaração População (diretamente no código, não pela entrada do úsuario)
-
-    printf("\nComparação de cartas (Atributo: População):\n");
-    printf("\nCarta 1 - %s: %lu\n", nome_cidade_a01, populacao_a01);
-    printf("Carta 2 - %s: %lu\n", nome_cidade_a02, populacao_a02);
-    printf("Resultado: ");
-    if (populacao_a01 > populacao_a02){
-        printf("Carta 1 (%s) venceu!\n", nome_cidade_a01);
+    // Menu de escolha do atributo para comparação
+    printf("Escolha um atributo:\n");
+    printf("1. Populacão\n");
+    printf("2. Área\n");
+    printf("3. PIB\n");
+    printf("4. Número de Pontos Turísticos\n");
+    printf("5. Densidade demográfica\n");
+    int escolhaJogador;
+    scanf("%d", &escolhaJogador);
+    
+    // Imprimindo a saída final de comparação
+    printf("\n%s X %s\n", nome_cidade_a01, nome_cidade_a02);
+    printf("Atributo escolhido: ");
+    switch (escolhaJogador)
+    {
+    case 1:
+        printf("População\n");
+        break;
+    case 2:
+        printf("Área\n");
+        break;
+    case 3:
+        printf("PIB\n");
+        break;
+    case 4:
+        printf("Número de Pontos Turísticos\n");
+        break;
+    case 5:
+        printf("Densidade demográfica\n");
+        break;
+    default:
+        printf("Opcao invalida.\n");
+        break;
     }
-    if (populacao_a01 == populacao_a02){
-        printf("Empate!\n");
+
+    if (escolhaJogador == 1){
+        printf("População %s: %lu\nPopulação %s: %lu\n", nome_cidade_a01, populacao_a01, nome_cidade_a02, populacao_a02);
+    }else if(escolhaJogador == 2){
+        printf("Área %s: %.2f\nÁrea %s: %.2f\n", nome_cidade_a01, area_a01, nome_cidade_a02, area_a02);
+    }else if(escolhaJogador == 3){
+        printf("PIB %s: %.2f\nPIB %s: %.2f\n", nome_cidade_a01, pib_a01, nome_cidade_a02, pib_a02);
+    }else if(escolhaJogador == 4){
+        printf("Número de pontos turísticos %s: %d\nNúmero de pontos turísticos %s: %d\n", 
+            nome_cidade_a01, nPontosTuristicos_a01, nome_cidade_a02, nPontosTuristicos_a02);
+    }else if(escolhaJogador == 5){
+        printf("Densidade demográfica %s: %.2f\nDensidade demográfica %s: %.2f\n", nome_cidade_a01, densidade_1, nome_cidade_a02, densidade_2);
+    }else{
+        printf("Opção inválida");
     }
-    if (populacao_a01 < populacao_a02){
-        printf("Carta 2 (%s) venceu!\n", nome_cidade_a02);
+
+    switch (escolhaJogador)
+    {
+    case 1:
+        imprime_comparacao(resultado_populacao);
+        break;
+    case 2:
+        imprime_comparacao(resultado_area);
+        break;
+    case 3:
+        imprime_comparacao(resultado_pib);
+        break;
+    case 4:
+        imprime_comparacao(resultado_pontosTuristicos);
+        break;
+    case 5:
+        imprime_comparacao(resultado_densidade);
+        break;
+    default:
+        printf("Opcao invalida.\n");
+        break;
     }
-
-
-
-
-
 }
 
+// Função retorna o resultado da divisão
 float divide(float x, float y)
 {
     float divisao = x / y;
     return divisao;
 }
 
+// Função usada para comparação dos atributos
 int compara(float x, float y)
 {
     int retorno = 0;
-    if (x > y)
+    if (x > y){
         retorno = 1;
-
-    if (x < y){
+    }else if (x < y){
         retorno = 0;
     }else{
         retorno = 2;
@@ -202,11 +217,15 @@ int compara(float x, float y)
     return retorno;
 }
 
+// Função imprime a carta vencedora
 void imprime_comparacao(int x)
 {
-    if (x == 0)
-        printf("Carta 2 venceu!");
-
-    if (x == 1)
-    printf("Carta 1 venceu!");
+    if (x == 0){
+        printf("Carta 2 (%s) venceu!\n", nome_cidade_a02);
+    }else if (x == 1){
+        printf("Carta 1 (%s) venceu!\n", nome_cidade_a01);
+    }else{
+        printf("Empate!\n");
+    }
+        
 }
